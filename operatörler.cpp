cb67ee3c49f61d58f46b30971ddc433ac0d8d4d5
Nur Vuslat Görgün <98425923+nurvuslat@users.noreply.github.com>
2022-12-01 02:05:49 +0300

    Add files via upload

---
+++ operatörler.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+main(){
+	int a =10;
+	int b = 4;
+	int islem1, islem2, islem3, islem4, islem5;
+	islem1 = a + b;
+	islem2 = a - b;
+	islem3 = a * b;
+	islem4 = float(a) / b;
+	
+	cout<<("Toplama:") << islem1<<endl;
+	cout<<("Cikarma:") << islem2<<endl;
+	cout<<("Carpma:") << islem3<<endl;
+	cout<<("Bolme:") << islem4<<endl;
+}
---
+++ sabitler.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#define PI 3.14
+main(){
+	float r, alan;
+	cout<<"Yaricapi Girin:";
+	cin>> r;
+	alan = PI * r * r;
+	cout<<"Alan: "<< alan;
+}
